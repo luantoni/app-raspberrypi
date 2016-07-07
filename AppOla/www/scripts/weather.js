659cf4c75e959383ccf01a4b43263c00378fb37b
Luísa Antoni da Rocha <luisa.rocha@digitaldesk.com.br>
2016-07-07 14:06:39 -0300

    arquivos

---
+++ AppOla/www/scripts/weather.js
@@ -0,0 +1,45 @@
+﻿var OpenWeatherAppKey = "0c2995af981572aae8f711564e88b1db";
+
+function getWeatherWithZipCode() {
+    var queryString = "http://192.168.1.154:3000/unlock";
+    
+    $.getJSON(queryString, function (results) {
+        mostra(results);
+
+    }).fail(function (jqXHR) {
+        $('#error-msg').show();
+        $('#error-msg').text("Error retrieving data. ");
+    });
+
+    return false;
+}
+
+function mostra(results) {
+    console.log(result);
+}
+
+function showWeatherData(results) {
+
+    if (results.weather.length) {
+
+        $('#error-msg').hide();
+        $('#weather-data').show();
+
+        $('#title').text(results.name);
+        $('#temperature').text(results.main.temp);
+        $('#wind').text(results.wind.speed);
+        $('#humidity').text(results.main.humidity);
+        $('#visibility').text(results.weather[0].main);
+
+        var sunriseDate = new Date(results.sys.sunrise * 1000);
+        $('#sunrise').text(sunriseDate.toLocaleTimeString());
+
+        var sunsetDate = new Date(results.sys.sunset * 1000);
+        $('#sunset').text(sunsetDate.toLocaleTimeString());
+
+    } else {
+        $('#weather-data').hide();
+        $('#error-msg').show();
+        $('#error-msg').text("Error retrieving data. ");
+    }
+}
